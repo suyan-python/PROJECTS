0a060eaf54e7c3f0395d44aec9ea669763543ab2
suyan man amatya <srkamatya007@gmail.com>
2022-10-30 12:12:49 +0545

    enter

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from "./components/About/about";
 import Experience from "./components/Experience/experience";
 import Services from "./components/Services/services";
 import Portfolio from "./components/Portfolio/portfolio";
-import Testimonials from "./components/Testimonials/testimonials";
+// import Testimonials from "./components/Testimonials/testimonials";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
 
